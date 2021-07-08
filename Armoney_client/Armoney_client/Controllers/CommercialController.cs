4a09db4376959b8711bc1c9fa895dd243df750c4
or robin <orrobin5823@gmail.com>
2021-07-09 00:35:18 +0300

    first

---
+++ Armoney_client/Armoney_client/Controllers/CommercialController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armoney_client.Controllers
+{
+    public class CommercialController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Suggestions()
+        {
+            return View();
+        }
+        public IActionResult Lessons()
+        {
+            return View();
+        }
+    }
+}
---
+++ Armoney_client/Armoney_client/Controllers/MoneyController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Armoney_client.Controllers
+{
+    public class MoneyController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ProgressBar()
+        {
+            ViewBag.num = 10;
+            return View();
+        }
+        public IActionResult WhereIsMyMoney()
+        {
+            ViewBag.place = "bank";
+            ViewBag.money = 150;
+            return View();
+        }
+    }
+}
